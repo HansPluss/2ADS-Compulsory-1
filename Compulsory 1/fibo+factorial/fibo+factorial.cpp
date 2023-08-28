2d5004c43668218f5838cd2c1c9e06268e947ea8
Hans <111863567+HansPluss@users.noreply.github.com>
2023-08-28 09:27:24 +0200

    Changed for loops to recursive
    + removed useless code

--- Compulsory 1/fibo+factorial/fibo+factorial.cpp
+++ Compulsory 1/fibo+factorial/fibo+factorial.cpp
@@ -17,39 +17,17 @@ double result;
 
 int iterations;
 int userInput;
-void Factorial(int num);
-void Fibonacci(int iteration);
-void InverseFactorial(int iter);
+int Factorial(int num);
+int Fibonacci(int iteration);
+
 
 int main()
 {
         std::cout << "Hello there, it do be math tho\n";
-        std::cout << "Type the number you want to get the inverse factorial of \n";
-        std::cin >> factInput;
-        inverseFactorial = factInput;
-        if (factInput < 0) {
-            std::cout << "ERROR, Number can not be lower than 0! \n";
-
-        }
-        else if (factInput == 0) {
-            inverseFactorial = 1;
-           
-        }
-        else if (!cin) {
-            std::cout << "ERROR, cannot be string \n";
-            cin.clear();
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
-        else {
-            InverseFactorial(inverseFactorial);
-        }
-        
-        
-        cin.clear();
         std::cout << "Type the number you want to get the factorial of \n";
         std::cin >> input;
         if (input < 0) {
-            std::cout << "ERROR, Number can not be lower than 0! \n";
+            std::cout << "ERROR, Number can not be lower than 0 \n";
 
         }
         else if (!cin) {
@@ -62,60 +40,52 @@ int main()
             std::cout << result << "\n";
         }
         else {
-            Factorial(input);
+            
+            std::cout << Factorial(input) << "\n";
         }
-        std::cout << "Type the number of iterations of the sequence you want \n";
+        std::cout << "Type the iteration of the sequence you want \n";
         std::cin >> userInput;
         iterations = userInput;
-        if (iterations <= 0 || !cin) {
+        if (iterations < 0 || !cin) {
             std::cout << "ERORR can't be lower than 0 or string! \n";
             
             cin.clear();
             cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             
         }
+        else if (iterations == 0) {
+            std::cout << iterations << "\n";
+        }
         else {
-            Fibonacci(iterations);
+            
+            std::cout << Fibonacci(iterations) << "\n";
         }
         main();
     
 }
-void Factorial(int num)
+int Factorial(int num)
 {
-    factorial = num;
-    for (int i = factorial - 1; i > 0; i--) {
-
-
-
-        factorial *= i;
-
+    if (num > 1) {
+        return num *= Factorial(num - 1);
     }
+    else {
+        return 1;
+    }
+
+
     std::cout << factorial << " \n";
 
 }
-void Fibonacci(int iteration) {
+int Fibonacci(int iteration) {
 
-    for (int i = 0; i < iteration; i++) {
-
-        result = (number + lastNumber);
-        number = lastNumber;
-        lastNumber = result;
-
-
-        std::cout << result << "\n";
+    
+    if (iteration > 1) {
+        return  Fibonacci(iteration - 1) + Fibonacci(iteration - 2);
+    }
+    else {
+        return 1;
     }
 }
-void InverseFactorial(int iter)
-{
-    inverseFactorial = iter;
-    for (int i = inverseFactorial - 1; i > 0; i--) {
 
-        // simply takes the next number and divide it with the current one
-        //example 5!^-1 = 5/4/3/2/1
-        inverseFactorial /= i;
-
-    }
-    std::cout << inverseFactorial << "\n";
-}
 
 
